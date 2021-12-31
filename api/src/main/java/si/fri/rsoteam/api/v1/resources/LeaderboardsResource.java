63f4826b0a41eba230a9fea8079ddcc4244522de
creativ10 <mv5195@student.uni-lj.si>
2021-12-31 05:33:30 +0100

    added annotations for open-api. Fix dependencies

--- api/src/main/java/si/fri/rsoteam/api/v1/resources/LeaderboardsResource.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/resources/LeaderboardsResource.java
@@ -34,29 +34,70 @@ public class LeaderboardsResource {
     protected UriInfo uriInfo;
 
     @GET
+    @Operation(summary = "Get list of leaderboards", description = "Returns list of leaderboard enteries.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Returned successfully list leaderboard",
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = LeaderboardDto.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )
+    })
     public Response getLeaderboards() {
         return Response.ok(leaderboardsBean.getAllLeaderboards()).build();
     }
 
     @GET
     @Path("/{objectId}")
+    @Operation(summary = "Get leaderboard by id.", description = "Returns specific leaderboard.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Returned successfully specific leaderboard entery",
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = LeaderboardDto.class, type = SchemaType.ARRAY))
+            )
+    })
     public Response getLeaderboardById(@PathParam("objectId") Integer id) {
         return Response.ok(leaderboardsBean.getLeaderboard(id)).build();
     }
 
     @POST
+    @Operation(summary = "Create new leaderboard entery ", description = "Create new leaderboard entery")
+    @APIResponses({
+            @APIResponse(
+                    description = "Created successfully leaderboard entery",
+                    responseCode = "201",
+                    content = @Content(schema = @Schema(implementation = LeaderboardDto.class, type = SchemaType.ARRAY))
+            )
+    })
     public Response createLeaderboard(LeaderboardDto leaderboardDto) {
         return Response.status(201).entity(leaderboardsBean.createLeaderboard(leaderboardDto)).build();
     }
 
     @PUT
     @Path("{objectId}")
+    @Operation(summary = "Update leaderboard.", description = "Updates specific leaderboard by id.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Updated successfully leaderboard entery",
+                    responseCode = "201",
+                    content = @Content(schema = @Schema(implementation = LeaderboardDto.class, type = SchemaType.ARRAY))
+            )
+    })
     public Response updateLeaderboard(@PathParam("objectId") Integer id, LeaderboardDto eventDto) {
         return Response.status(201).entity(leaderboardsBean.updateLeaderboard(eventDto, id)).build();
     }
 
     @DELETE
     @Path("{objectId}")
+    @Operation(summary = "Delete leaderboard.", description = "Delete specific leaderboard by id.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Deleted successfully leaderboard entery",
+                    responseCode = "204",
+                    content = @Content(schema = @Schema(implementation = LeaderboardDto.class, type = SchemaType.ARRAY))
+            )
+    })
     public Response deleteEvent(@PathParam("objectId") Integer id) {
         leaderboardsBean.deleteLeaderboard(id);
         return Response.status(204).build();
