8249a7cc288f476d421a1b4e7cf0b5692c8f9ddd
Luka Galjot <luka.galjot@gmail.com>
2022-01-13 22:49:30 +0100

    hc openapi url

--- api/src/main/java/si/fri/rsoteam/api/v1/MicroserviceLeaderboards.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/MicroserviceLeaderboards.java
@@ -18,7 +18,7 @@ import javax.ws.rs.core.Application;
                 contact = @Contact(),
                 license = @License(name = "none")
         ),
-        servers = @Server(url = "http://localhost:8087"),
+        servers = @Server(url = "http://52.226.192.49/leaderboards"),
         security = @SecurityRequirement(name = "none"))
 @RegisterService
 @ApplicationPath("/v1")
